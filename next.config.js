5fb85723fe8dc4a7d9665398c6b63a163597aff8
Aurimar Jorge de Araujo Antunes Lopes <aurimardev@gmail.com>
2022-12-10 21:02:00 +0000

    init
    Signed-off-by: Aurimar Jorge de Araujo Antunes Lopes <aurimardev@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+});
+
+const nextConfig = withPWA({
   experimental: {
     appDir: true,
   },
-}
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
